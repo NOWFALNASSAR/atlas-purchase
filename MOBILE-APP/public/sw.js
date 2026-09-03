5729549e3e165226c2ec70fab5f09e8b7cb372f3
NOWFALNASSAR <75140018+NOWFALNASSAR@users.noreply.github.com>
2026-09-03 19:18:51 +0530

    Update sw.js

--- MOBILE-APP/public/sw.js
+++ MOBILE-APP/public/sw.js
@@ -80,3 +80,10 @@ self.addEventListener('fetch', e => {
 self.addEventListener('message', e => {
   if (e.data === 'skip-waiting') self.skipWaiting()
 })
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => new Response('Offline', { status: 503 }))
+  );
+});
